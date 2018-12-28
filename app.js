7ff1bddc1c5d5ad2038c83f695a0f20b4acb7672
JRocken <jp.rockenmacher@gmail.com>
2018-12-27 21:10:17 -0700

    first part done

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 new Vue({
         el: '#exercise',
         data: {
-            value: 0
+            value: 0,
+        },
+        computed: {
+            result: function(){
+                return this.value < 26 ? "keep rolling" : "slow that roll"
+            },
+            watch: {
+
+            }
         }
     });
